d9ba26269f4d61c273fba0dc782c7f89c1bea3a3
Mfikrilamin <muhd.fikrilamin@gmail.com>
2024-03-21 13:17:06 +0800

    Add controller, model, service, repo and application configuration

---
+++ inventory-service/src/main/java/com/microservicesproject/inventoryservice/util/DataLoader.java
@@ -0,0 +1,29 @@
+package com.microservicesproject.inventoryservice.util;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.microservicesproject.inventoryservice.model.Inventory;
+import com.microservicesproject.inventoryservice.repository.InventoryRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class DataLoader implements CommandLineRunner {
+    private final InventoryRepository inventoryRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Inventory inventory = new Inventory();
+        inventory.setSkuCode("iphone_13");
+        inventory.setQuantity(100);
+
+        Inventory inventory1 = new Inventory();
+        inventory1.setSkuCode("iphone_13_red");
+        inventory1.setQuantity(0);
+
+        inventoryRepository.save(inventory);
+        inventoryRepository.save(inventory1);
+    }
+}
